511690c694671bb04163f534f31a9ad2f37b5c9f
Dakota States <dakota.states@gmail.com>
2020-02-12 21:25:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   a = sentence.split
   b = []
-  a.each {|word| b << "#{word.reverse}"
+  a.each {|word| b << "#{word.reverse}"}
   b
   a.collect {|word| "#{word.reverse}"}
   
